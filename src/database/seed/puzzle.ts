da28ea51ffcc2ad433864184a06937e080e883a4
Valerian Clerc <valerian.clerc@gmail.com>
2021-02-15 17:14:00 -0500

    Add categories to seed

--- src/database/seed/puzzle.ts
+++ src/database/seed/puzzle.ts
@@ -4,25 +4,29 @@ export const samplePuzzles: IPuzzle[] = [
     {
         title: "3",
         description: "this is another another another puzzle",
-        answer: "2",
+        answer: "3",
+        category: "Uncategorized",
         next: [],
     },
     {
         title: "2",
         description: "this is another another puzzle",
         answer: "2",
+        category: "Uncategorized",
         next: [],
     },
     {
         title: "1",
         description: "this is another puzzle",
         answer: "1",
+        category: "Uncategorized",
         next: ["2", "3"],
     },
     {
         title: "0",
         description: "this is a puzzle",
         answer: "0",
+        category: "Uncategorized",
         next: ["1"],
     },
 ]
